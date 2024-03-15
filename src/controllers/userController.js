3ea2b7086c1c32ac9f6d15bb6458b594d4a42a50
HUNGNS <syhung@aladintech.co>
2024-03-16 02:12:23 +0700

    hotfix/ check account

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,107 +4,107 @@ const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 module.exports = {
-    login: (req, res) => {
-        const { username, password } = req.body;
-        userModel.getUsers(async (err, users) => {
-            if (err) throw err;
-            const user = users.find((user) => {
-                return user.phone === username;
-            });
-            if (user) {
-                const passwordMatch = await bcrypt.compare(
-                    password,
-                    user.password
-                );
-                if (passwordMatch) {
-                    const token = jwt.sign(
-                        {
-                            id: user.id,
-                            phone: user.phone,
-                        },
-                        process.env.JWT_SECRET,
-                        { expiresIn: "15s" }
-                    );
-                    const refreshToken = jwt.sign(
-                        {
-                            id: user.id,
-                            phone: user.phone,
-                        },
-                        process.env.REFRESH_SECRET,
-                        {
-                            expiresIn: "30d",
-                        }
-                    );
-                    res.json({
-                        status: 200,
-                        data: {
-                            id: user.id,
-                            phone: user.phone,
-                            token: token,
-                            refreshToken: refreshToken,
-                        },
-                    });
-                } else {
-                    res.json({
-                        status: 400,
-                        data: {
-                            message: "Invalid phone or password",
-                        },
-                    });
-                }
-            } else {
-                res.json({
-                    status: 400,
-                    data: {
-                        message: "Invalid phone or password",
-                    },
-                });
+  login: (req, res) => {
+    const { username, password } = req.body;
+    userModel.getUsers(async (err, users) => {
+      if (err) throw err;
+      const user = users.find((user) => {
+        return user.phone === username;
+      });
+      if (user) {
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (passwordMatch) {
+          const token = jwt.sign(
+            {
+              id: user.id,
+              phone: user.phone,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "15s" }
+          );
+          const refreshToken = jwt.sign(
+            {
+              id: user.id,
+              phone: user.phone,
+            },
+            process.env.REFRESH_SECRET,
+            {
+              expiresIn: "30d",
             }
+          );
+          res.json({
+            status: 200,
+            data: {
+              id: user.id,
+              phone: user.phone,
+              token: token,
+              refreshToken: refreshToken,
+            },
+          });
+        } else {
+          res.json({
+            status: 400,
+            data: {
+              message: "Invalid phone or password",
+            },
+          });
+        }
+      } else {
+        res.json({
+          status: 400,
+          data: {
+            message: "Invalid phone or password",
+          },
         });
-    },
-    addUser: (req, res) => {
-        const { phone, password, fullName, email } = req.body;
-        userModel.getUsers(async (err, users) => {
-            if (err) throw err;
+      }
+    });
+  },
+  addUser: (req, res) => {
+    const { phone, password, fullName, email } = req.body;
+    userModel.getUsers(async (err, users) => {
+      if (err) throw err;
 
-            const checkPhone = users.some((user) => user.phone !== phone);
-            if (checkPhone) {
-                const hashedPassword = await bcrypt.hash(password, 10);
-                userModel.addUser(
-                    {
-                        phone: phone,
-                        password: hashedPassword,
-                        fullName: fullName,
-                        email: email,
-                    },
-                    (err) => {
-                        if (err) throw err;
-                        res.json({
-                            status: 200,
-                            data: {
-                                message: "successfully",
-                            },
-                        });
-                    }
-                );
-            } else {
-                res.json({
-                    status: 400,
-                    data: {
-                        message: "Phone already exists",
-                    },
-                });
-            }
-        });
-    },
-    getUsers: (req, res) => {
-        console.log("🚀 ~ req:", req.body);
-        userModel.getUsers((err, users) => {
+      const checkPhone = users.some((user) => user.phone !== phone);
+      if (checkPhone) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        userModel.addUser(
+          {
+            phone: phone,
+            password: hashedPassword,
+            fullName: fullName,
+            email: email,
+          },
+          (err) => {
             if (err) throw err;
             res.json({
-                status: 200,
-                data: { isCheck: true },
+              status: 200,
+              data: {
+                message: "successfully",
+              },
             });
+          }
+        );
+      } else {
+        res.json({
+          status: 400,
+          data: {
+            message: "Phone already exists",
+          },
         });
-    },
+      }
+    });
+  },
+  getUsers: (req, res) => {
+    const { phone } = req.body;
+    userModel.getUsers((err, users) => {
+      if (err) throw err;
+      if (users) {
+        const result = users.some((item) => item.phone === phone);
+        res.json({
+          status: 200,
+          data: { isCheck: result },
+        });
+      }
+    });
+  },
 };
